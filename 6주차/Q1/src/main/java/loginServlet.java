110ebc90f8c53c3138870029a0e19b27bf4ce39b
YeahOut <131945340+YeahOut@users.noreply.github.com>
2023-10-10 14:31:57 +0900

    feat : 문제1

---
+++ 6주차/Q1/src/main/java/loginServlet.java
@@ -0,0 +1,60 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		
+		String id = request.getParameter("name");
+		String pw = request.getParameter("phone");
+		String ad = request.getParameter("address");
+		
+		response.setContentType("text/html;charset=euc-kr");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<HTML>");
+		out.println("<HEAD></HEAD");
+		out.println("<BODY>");
+		out.println("NAME =" + id + "<br>");
+		out.println("PHONE =" + pw + "<br>");
+		out.println("ADDRESS =" + ad + "<br>");
+		out.println("</BODY>");
+		out.println("</HTML>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ 개인실습/practice/src/main/java/Login_Servlet.java
@@ -0,0 +1,55 @@
+
+
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Login_Servlet
+ */
+public class Login_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login_Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		
+		
+        String color = request.getParameter("color");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<HTML>");
+		out.println("<HEAD></HEAD>");
+		out.println("<BODY>");
+		out.println("단일 선택 항목 색상 = " + color + "<br/><br/>");
+		out.println("</BODY>");
+		out.println("</HTML>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
