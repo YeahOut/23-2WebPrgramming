d5ad16661654b3f1bde7c7328d3af49c88597000
YeahOut <131945340+YeahOut@users.noreply.github.com>
2023-10-20 03:20:04 +0900

    feat : MVC

---
+++ 7주차/MVC/src/main/java/com/example/web/BeerSelect.java
@@ -0,0 +1,49 @@
+package com.example.web;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class BeerSelect
+ */
+public class BeerSelect extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BeerSelect() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("Beer Seletion Advice<br>");
+		String c = request.getParameter("color");
+		out.println("<br>Got Beer Color<br>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
